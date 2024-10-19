db5a8c9da9af53ac12e00546dc9385569e2f6613
Samaholicc <samaholiccs@gmail.com>
2024-10-19 23:45:07 +0200

    last1 flux RSS

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -22,7 +22,7 @@ const Services = () => {
     const fetchRSS = async () => {
       try {
         const response = await fetch(
-          "https://api.rss2json.com/v1/api.json?rss_url=https://www.aitrends.com/feed/"
+          "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml"
         );
         const data = await response.json();
         console.log(data); // Vérifie la réponse ici
