7108a6c179e44886ef38413e592e79ddc9e94e1c
Samaholicc <samaholiccs@gmail.com>
2024-10-26 23:06:53 +0200

    Updatescreenshots

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,11 +12,11 @@ const Work = () => {
       </h2>
 
       <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
-        <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
+        <div className="aspect-square bg-[url('./assets/task.jpg')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
           <a href="https://github.com/samaholicc/systeme-de-livraison">
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
               <div>
-                <h2 className="font-semibold">System de livraison</h2>
+                <h2 className="font-semibold">Gestionnaire des tâches </h2>
                 <p className="text-sm text-gray-700">Technologie : Laravel</p>
               </div>
               <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
