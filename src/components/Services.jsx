e016832dc55eac8e791d8071f50afb03dd478328
Samaholicc <samaholiccs@gmail.com>
2024-10-26 14:51:32 +0200

    last1 flux RSS

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,10 +12,9 @@ import aiBook6 from "../assets/The_AI_Classroom_The_Ultimate_Guide_to_Artificial
 
 const Services = () => {
   const [showModal, setShowModal] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  
-  // Liste des PDF sur l'IA
-  const pdfFiles = [
+  const [selectedService, setSelectedService] = useState(null);
+   // Liste des PDF sur l'IA
+   const pdfFiles = [
     { name: "AI for Absolute Beginners by Oliver Theobald", url: aiBook1 },
     { name: "Artificial Intelligence: The Ultimate Guide to AI", url: aiBook2 },
     { name: "ChatGPT Decoded by David Wiens", url: aiBook3 },
@@ -24,90 +23,77 @@ const Services = () => {
     { name: "The AI Classroom by Brad Weinstein", url: aiBook6 },
   ];
 
-  const handleShowDetails = (event) => {
-    setSelectedEvent(event);
+
+  const handleShowDetails = (service) => {
+    setSelectedService(service);
     setShowModal(true);
   };
 
   const handleCloseModal = () => {
     setShowModal(false);
-    setSelectedEvent(null);
+    setSelectedService(null);
   };
-
-  // Timeline events
-  const events = [
-    { year: '1950-1980', description: 'Les premiers concepts d\'intelligence artificielle émergent avec des travaux sur les algorithmes et la logique.' },
-    { year: '1980-2010', description: 'Introduction des réseaux de neurones simples et des premières applications de l\'apprentissage automatique. L\'accent est mis principalement sur le raisonnement et la classification.' },
-    { year: '2014', description: 'Ian Goodfellow propose les réseaux antagonistes génératifs (GAN), permettant de générer des images réalistes à partir de bruit.' },
-    { year: '2018', description: 'Lancement de modèles de langage comme BERT et GPT, qui révolutionnent le traitement du langage naturel.' },
-    { year: '2020', description: 'GPT-3 est publié, démontrant des capacités impressionnantes en génération de texte.' },
-    { year: '2021-2023', description: 'L\'IA générative trouve des applications dans la création d\'art, la musique, la rédaction, et même dans le codage, avec des outils comme DALL-E et Codex.' },
-  ];
 
   return (
     <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
       <h2 className="text-center text-5xl font-Ovo">Veille Technologique</h2>
 
       <div className="gap-8 my-10">
-        <motion.div
-          whileHover={{ scale: 1.05 }}
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.1 }}
-          className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-xl cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
-        >
-          <img
-            src={interestImage}
-            alt="Image representing Intelligence Artificielle"
-            className="w-22 h-22 mx-auto mb-4 rounded-full"
-          />
-
-            <h3 className="text-lg my-4 text-center text-gray-700 dark:text-white">Generative AI</h3>
-          <p className="text-center text-gray-600 dark:text-white/80 mb-4">
+        <div className="gap-8">
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-xl cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
+          >
+            <img
+              src={interestImage}
+              alt="Image representing Intelligence Artificielle"
+              className="w-22 h-22 mx-auto mb-4 rounded-full"
+            />
+            <h3 className="text-lg my-4 text-center text-gray-700 dark:text-white">Generative AI </h3>
+            <p className="text-center text-gray-600 dark:text-white/80 mb-4">
             Les machines sont aujourd’hui aussi compétentes que l’humain dans la création de tous types de concepts et de contenus. Cela est possible grâce à la technologie de calculs informatiques de données produits par la Generative AI.
-          </p>
-          <div className="text-center">
-            <a
-              href="https://en.wikipedia.org/wiki/Generative_artificial_intelligence"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              Wikipedia
-            </a>
-            <br />
-            <a
-              href="https://www.techtarget.com/searchenterpriseai/definition/generative-AI"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              Techtarget
-            </a>
-          </div>
-
-          {events.map((event, index) => (
-            <motion.div key={index} className="mt-6">
-              <h4 className="text-md font-semibold text-gray-700">{event.year}</h4>
-              <p className="text-gray-600 dark:text-white/80 mb-2">{event.description}</p>
-              <button
-                className="mt-4 px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 mx-auto block"
-                onClick={() => handleShowDetails({
-                  title: event.year,
-                  description: event.description,
-                  // You can include more data if needed
-                })}
+            </p>
+            <div className="text-center">
+              <a
+                href="https://en.wikipedia.org/wiki/Generative_artificial_intelligence"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
               >
-                Voir les détails
-              </button>
-            </motion.div>
-          ))}
-        </motion.div>
+              Wikipedia
+              </a>
+              <br />
+              <a
+                href="https://www.techtarget.com/searchenterpriseai/definition/generative-AI"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                Techtarget
+              </a>
+            </div>
+            <button
+              className="mt-4 px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 mx-auto block"
+              onClick={() =>
+                handleShowDetails({
+                  title: "IA génerative",
+                  details: "Détails sur l'IA génerative",
+                  advantages: ["Automatisation", "Personnalisation"],
+                })
+              }
+            >
+              Voir les détails
+            </button>
+          </motion.div>
+        </div>
 
         <div className="mt-8">
           <h2 className="text-center text-3xl font-semibold mb-6">Livres sur l'IA (PDF)</h2>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2">
-            {pdfFiles.map((pdf) => (
+            {pdfFiles.map((pdf, index) => (
               <div
                 key={pdf.name} // Use pdf.name instead of index for better key
                 className="border border-gray-300 rounded-lg p-4 shadow-md text-center"
@@ -127,8 +113,8 @@ const Services = () => {
         </div>
       </div>
 
-      {/* Popup Modal for Event Details */}
-      {showModal && selectedEvent && (
+      {/* Popup Modal */}
+      {showModal && selectedService && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <motion.div
             initial={{ opacity: 0, scale: 0.8 }}
@@ -137,9 +123,15 @@ const Services = () => {
             className="bg-white rounded-lg p-8 w-[90%] md:w-[40%] shadow-lg"
           >
             <h3 className="text-2xl font-semibold text-gray-700 mb-4">
-              Détails sur {selectedEvent.title}
+              Détails sur {selectedService.title}
             </h3>
-            <p className="text-gray-600 text-sm mb-4">{selectedEvent.description}</p>
+            <p className="text-gray-600 text-sm mb-4">{selectedService.details}</p>
+            <h4 className="text-xl font-semibold text-gray-700 mb-2">Avantages</h4>
+            <ul className="space-y-2 text-gray-600 text-sm">
+              {selectedService.advantages.map((advantage, idx) => (
+                <li key={idx}>• {advantage}</li>
+              ))}
+            </ul>
             <button
               className="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
               onClick={handleCloseModal}
@@ -150,7 +142,6 @@ const Services = () => {
         </div>
       )}
     </div>
-  
   );
 };
 
