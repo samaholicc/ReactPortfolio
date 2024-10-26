6bb8fe4da68cffb39abeb3567f0fb26d5fb7543a
Samaholicc <samaholiccs@gmail.com>
2024-10-26 16:22:16 +0200

    logo source

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,10 +16,13 @@ import googleCloudLogo from "../assets/google-cloud-logo.png";
 import openAiLogo from "../assets/open-ai-logo.png"; 
 import deepMindLogo from "../assets/deep-mind-logo.png"; 
 import theGradientLogo from "../assets/the-gradient-logo.png"; 
+import cnilLogo from "../assets/cnil-logo.png"; 
+
 const Services = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedService, setSelectedService] = useState(null);
-   // Liste des PDF sur l'IA
+
+  // List of AI PDF files
   const pdfFiles = [
     { name: "AI for Absolute Beginners by Oliver Theobald", url: aiBook1 },
     { name: "Artificial Intelligence: The Ultimate Guide to AI", url: aiBook2 },
@@ -29,7 +32,6 @@ const Services = () => {
     { name: "The AI Classroom by Brad Weinstein", url: aiBook6 },
   ];
 
-  
   const handleShowDetails = (service) => {
     setSelectedService(service);
     setShowModal(true);
@@ -39,6 +41,7 @@ const Services = () => {
     setShowModal(false);
     setSelectedService(null);
   };
+
   const timelineEvents = [
     { year: '1950-1980', description: "Les premiers concepts d'intelligence artificielle émergent avec des travaux sur les algorithmes et la logique." },
     { year: '1980-2010', description: "Introduction des réseaux de neurones simples et des premières applications de l'apprentissage automatique." },
@@ -53,112 +56,46 @@ const Services = () => {
       <h2 className="text-center text-5xl font-Ovo">Veille Technologique</h2>
 
       <div className="gap-8 my-10">
-        <div className="gap-8">
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.1 }}
-            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-xl cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
-          >
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-xl cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow white">
             <img
               src={interestImage}
               alt="Image representing Intelligence Artificielle"
               className="w-22 h-22 mx-auto mb-4 rounded-full"
             />
-            <h2 className="text-center text-3xl font-semibold mb-6">Generative AI </h2>
+            <h2 className="text-center text-3xl font-semibold mb-6">Generative AI</h2>
             <p className="text-center text-gray-600 dark:text-white/80 mb-4">
-            Les machines sont aujourd’hui aussi compétentes que l’humain dans la création de tous types de concepts et de contenus. Cela est possible grâce à la technologie de calculs informatiques de données produits par la Generative AI.
+              Les machines sont aujourd’hui aussi compétentes que l’humain dans la création de tous types de concepts et de contenus. Cela est possible grâce à la technologie de calculs informatiques de données produits par la Generative AI.
             </p>
-            <div className="text-center">
-            <div className="mt-8">
-  <h3 className="text-center text-3xl font-semibold mb-6">Sources d'actualités sur l'IA Générative</h3>
-  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
-    <div className="flex items-center justify-center mb-2">
-      <img src={wikipediaLogo} alt="Wikipedia Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://en.wikipedia.org/wiki/Generative_artificial_intelligence"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        Wikipedia
-      </a>
-    </div>
-    <div className="flex items-center justify-center mb-2">
-      <img src={techTargetLogo} alt="TechTarget Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://www.techtarget.com/searchenterpriseai/definition/generative-AI"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        TechTarget
-      </a>
-    </div>
-    <div className="flex items-center justify-center mb-2">
-      <img src={salesforceLogo} alt="SalesForce Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://www.salesforce.com/fr/resources/definition/ia-generative/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        SalesForce
-      </a>
-    </div>
-    <div className="flex items-center justify-center mb-2">
-      <img src={googleCloudLogo} alt="Google Cloud Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://cloud.google.com/use-cases/generative-ai?hl=fr"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        Google Cloud
-      </a>
-      
-    </div>
-    <div className="flex items-center justify-center mb-2">
-      <img src={openAiLogo} alt="Open AI Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://openai.com/news/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        OpenAI Blog
-      </a>
-      
-    </div>
-    <div className="flex items-center justify-center mb-2">
-      <img src={theGradientLogo} alt="The Gradient Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://thegradient.pub/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        The Gradient  
-      </a>
-      <div className="flex items-center justify-center mb-2">
-      <img src={deepMindLogo} alt="Deep Mind Logo" className="h-6 w-6 mr-2" />
-      <a
-        href="https://deepmind.google/discover/blog/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        DeepMind Blog
-      </a>
-      
-    </div>
-    </div>
-    
-  </div>
-</div>
+            <h3 className="text-center text-3xl font-semibold mb-6">Sources d'actualités sur l'IA Générative</h3>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+              {[
+                { logo: wikipediaLogo, name: "Wikipedia", link: "https://en.wikipedia.org/wiki/Generative_artificial_intelligence" },
+                { logo: techTargetLogo, name: "TechTarget", link: "https://www.techtarget.com/searchenterpriseai/definition/generative-AI" },
+                { logo: salesforceLogo, name: "SalesForce", link: "https://www.salesforce.com/fr/resources/definition/ia-generative/" },
+                { logo: googleCloudLogo, name: "Google Cloud", link: "https://cloud.google.com/use-cases/generative-ai?hl=fr" },
+                { logo: openAiLogo, name: "OpenAI Blog", link: "https://openai.com/news/" },
+                { logo: theGradientLogo, name: "The Gradient", link: "https://thegradient.pub/" },
+                { logo: deepMindLogo, name: "DeepMind Blog", link: "https://deepmind.google/discover/blog/" },
+              ].map(({ logo, name, link }) => (
+                <div key={name} className="flex items-center justify-center mb-2">
+                  <img src={logo} alt={`${name} Logo`} className="h-6 w-6 mr-2" />
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    {name}
+                  </a>
+                </div>
+              ))}
+            </div>
 
-</div>
             {/* Button to show details */}
             <button
               className="mt-4 px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 mx-auto block"
@@ -179,9 +116,9 @@ const Services = () => {
         <div className="mt-8">
           <h2 className="text-center text-3xl font-semibold mb-6">Livres sur l'IA (PDF)</h2>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2">
-            {pdfFiles.map((pdf, index) => (
+            {pdfFiles.map((pdf) => (
               <div
-                key={pdf.name} // Use pdf.name instead of index for better key
+                key={pdf.name}
                 className="border border-gray-300 rounded-lg p-4 shadow-md text-center"
               >
                 <h3 className="text-lg font-semibold mb-2">{pdf.name}</h3>
@@ -198,61 +135,56 @@ const Services = () => {
           </div>
         </div>
       </div>
-
+      )
       {/* Popup Modal */}
       {showModal && selectedService && (
-  <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-    <motion.div
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.3 }}
-      className="bg-white rounded-lg p-8 w-[90%] md:w-[70%] lg:w-[50%] xl:w-[40%] max-h-screen overflow-auto shadow-lg"
-    >
-      <h3 className="text-2xl font-semibold text-gray-700 mb-4">
-        {selectedService.title}
-      </h3>
-      <p className="text-gray-600 text-sm mb-4">{selectedService.details}</p>
-      
-      <h4 className="text-xl font-semibold text-gray-700 mb-2">Avantages</h4>
-      <ul className="space-y-2 text-gray-600 text-sm">
-        {selectedService.advantages.map((advantage, idx) => (
-          <li key={idx}>• {advantage}</li>
-        ))}
-        <br/>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3 }}
+            className="bg-white rounded-lg p-8 w-[90%] md:w-[70%] lg:w-[50%] xl:w-[40%] max-h-screen overflow-auto shadow-lg"
+          >
+          <h3 className="text-2xl font-semibold text-gray-700 mb-4">
+          {selectedService.title}
+        </h3>
+        <p className="text-gray-600 text-sm mb-4">{selectedService.details}</p>
+        
+        <h4 className="text-xl font-semibold text-gray-700 mb-2">Avantages</h4>
+        <ul className="space-y-2 text-gray-600 text-sm">
+          {selectedService.advantages.map((advantage, idx) => (
+            <li key={idx}>• {advantage}</li>
+          ))}
+        </ul>
 
-      </ul>
-
-    {/* Emergence Section - Animated Timeline */}
-    <div className="text-gray-600 text-sm mb-4">
-              <h4 className="text-xl font-semibold text-gray-700 mb-2">Émergence</h4>
-              <p>Voici une chronologie des événements marquants qui ont conduit à l'émergence de l'IA générative :</p>
-              <ol className="list-decimal list-inside space-y-2">
-                {selectedService.emergence.map((event, index) => (
-                  <motion.li
-                    key={index}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5, delay: index * 0.2 }}
-                    className="text-gray-600"
-                  >
-                    <strong>{event.year} :</strong> {event.description}
-                  </motion.li>
-                ))}
-              </ol>
-            </div>
-
-            <button
-              className="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
-              onClick={handleCloseModal}
-            >
-              Fermer
-            </button>
-          </motion.div>
+        {/* Emergence Section - Animated Timeline */}
+        <div className="text-gray-600 text-sm mb-4">
+          <h4 className="text-xl font-semibold text-gray-700 mb-2">Émergence</h4>
+          <p>Voici une chronologie des événements marquants qui ont conduit à l'émergence de l'IA générative :</p>
+          <ol className="list-decimal list-inside space-y-2">
+            {selectedService.emergence.map((event, index) => (
+              <motion.li
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                className="text-gray-600"
+              >
+                <strong>{event.year} :</strong> {event.description}
+              </motion.li>
+            ))}
+          </ol>
         </div>
-      )}
-  
+
+        <button
+          className="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+          onClick={handleCloseModal}
+        >
+          Fermer
+        </button>
+      </motion.div>
     </div>
-  );
-};
+  )}
+}
 
 export default Services;
