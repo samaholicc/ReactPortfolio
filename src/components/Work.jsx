8360e74805ca5c57d60903cd1f3e89ec6ea1d023
Samaholicc <samaholiccs@gmail.com>
2024-10-26 18:09:57 +0200

    carousel pdf

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,15 +6,10 @@ import right_arrow_blod_dark from "../assets/right-arrow-bold-dark.png";
 const Work = () => {
   return (
     <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Mon portfolio</h4>
+      
       <h2 className="text-center text-5xl font-Ovo">
-        Mes derniers Réalisation
+        Mes dernières réalisations
       </h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
-        Bienvenue dans mon portfolio de développement web ! Explorez une
-        collection de projets mettant en avant mon expertise en développement
-        front-end.
-      </p>
 
       <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
         <div className="aspect-square bg-[url('./assets/work-1.png')] bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
