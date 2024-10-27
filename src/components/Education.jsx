7407218db317eee592ff2009480cda18bfc711e0
Samaholicc <samaholiccs@gmail.com>
2024-10-27 14:00:46 +0100

    revision

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -100,38 +100,39 @@ const Education = () => {
       {/* Modal Popup */}
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <motion.div
-      key={index}
-      initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
-      whileInView={{ opacity: 1, x: 0, scale: 1.05 }}
-      viewport={{ once: true }}
-      transition={{
-      type: "spring",
-      stiffness: 50,
-      duration: 0.5,
-      delay: index * 0.2,
-  }}
-  className={`flex flex-col md:flex-row items-center ${
-    index % 2 === 0 ? "md:justify-start" : "md:justify-end"
-  } w-full ${index === 1 ? "md:mt-4" : ""}` /* Add this line */}
->
-            <h3 className="text-2xl font-semibold text-gray-700 mb-4">
-              Options
-            </h3>
-            <ul className="space-y-2 text-gray-600">
-              {selectedOptions.map((option, idx) => (
-                <li key={idx} className="text-lg">
-                  • {option}
-                </li>
-              ))}
-            </ul>
-            <button
-              className="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
-              onClick={handleCloseModal}
-            >
-              Fermer
-            </button>
-          </motion.div>
+         <motion.div
+          key={index}
+          initial={{ opacity: 0, x: index % 2 === 0 ? -100 : 100 }}
+          whileInView={{ opacity: 1, x: 0, scale: 1.05 }}
+          viewport={{ once: true }}
+          transition={{
+          type: "spring",
+          stiffness: 50,
+          duration: 0.5,
+          delay: index * 0.2,
+          }}
+        className={`flex flex-col md:flex-row items-center ${
+          index % 2 === 0 ? "md:justify-start" : "md:justify-end"
+        } w-full ${index === 1 ? "ml-8" : ""} ${index === 1 ? "md:mt-4" : ""}`} // Added "ml-8" for left margin for the second card
+      >
+        <h3 className="text-2xl font-semibold text-gray-700 mb-4">
+          Options
+        </h3>
+        <ul className="space-y-2 text-gray-600">
+          {selectedOptions.map((option, idx) => (
+            <li key={idx} className="text-lg">
+              • {option}
+            </li>
+          ))}
+        </ul>
+          <button
+          className="mt-6 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+          onClick={handleCloseModal}
+        >
+          Fermer
+          </button>
+        </motion.div>
+
         </div>
       )}
     </div>
