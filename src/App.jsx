7d704e63386908a23224883129e0a9aaa96e6797
Samaholicc <samaholiccs@gmail.com>
2024-10-28 12:10:12 +0100

    rev

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Work from "./components/Work";
-import Services from "./components/Services";
 import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
