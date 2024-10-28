d5e292177a64d118659010ef9894bae0e286e391
Samaholicc <samaholiccs@gmail.com>
2024-10-28 12:04:06 +0100

    rev

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
 
   return (
     <div id="projects" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Mes Projets</h4>
-      <h2 className="text-center text-5xl font-Ovo">Derniers Projets</h2>
+    
+      <h2 className="text-center text-5xl font-Ovo">Projets</h2>
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Voici quelques-uns de mes projets récents, mettant en valeur mon
         expertise dans le développement web et d'applications mobiles.
