677080a67f3ef5d45be8b78e241c7cec509c92f0
Samaholicc <samaholiccs@gmail.com>
2024-11-07 19:01:20 +0100

    vt

--- src/components/VeilleTechnologique.jsx
+++ src/components/VeilleTechnologique.jsx
@@ -43,12 +43,13 @@ const VeilleTechnologique = () => {
         <Card 
           title="React" 
           image={sampleImage5} 
-          description="React est une bibliothèque JavaScript populaire utilisée pour créer des interfaces utilisateur, notamment pour les applications à page unique. Développée par Facebook, elle permet de créer des composants UI réutilisables, de gérer efficacement l'état des applications via un DOM virtuel et d'utiliser JSX pour écrire du code semblable à HTML."
+          description="React est une bibliothèque JavaScript populaire utilisée pour créer des interfaces utilisateur, notamment pour les applications à page unique. Développée par Facebook, elle permet de créer des composants UI réutilisables, de gérer efficacement l'état des applications via un DOM virtuel et d'utiliser JSX pour écrire du code semblable à HTML. J'utilise Inoreader comme outil de veille technologique pour rester informé des dernières tendances et nouveautés dans le domaine."
         />
             <div className="flex-1 max-w-3xl">
             <h3 className="text-3xl mb-4">Mon flux RSS INOREADER</h3>
-            <img src={Inoreader} alt="Inoreader" />
+            
             <div className="flex items-center justify-between p-4 border border-gray-300 rounded-lg shadow-lg">
+              <img src={Inoreader} alt="Inoreader" />
               <button className="text-2xl" onClick={prevArticle}>‹</button>
               <div className="text-center flex-1">
               {articles.length === 0 ? (
@@ -80,7 +81,7 @@ const Card = ({ title, image, description }) => {
     <div className="relative w-full max-w-md p-6 border border-gray-300 rounded-lg shadow-lg">
       <h2 className="text-2xl font-semibold mb-2">{title}</h2>
       <img src={image} alt={`${title} Icon`} className="w-full h-auto rounded mb-4" />
-      <p className="text-gray-700">{description}</p>
+      <p className="text-white-700">{description}</p>
     </div>
   );
 };
