3bed7fb23f7bd6a522ad120af4942d4df37de293
Samaholicc <samaholiccs@gmail.com>
2024-11-20 11:29:33 +0100

    final

--- src/components/EcoleEntreprise.jsx
+++ src/components/EcoleEntreprise.jsx
@@ -6,26 +6,26 @@ import ndgcImage from '../assets/NDDGC.jpg';      // **Verify this path!**
 
 const EcoleEntreprise = () => {
   return (
-    <div style={styles.wrapper}>
+    <div id ="EcoleEntreprise" style={styles.wrapper}>
       <h2 style={styles.heading}>École et Entreprise</h2>
       <div style={styles.container}>
         <Card
           title="Webitech Paris"
-          link="https://www.webitech.fr/"
+          link="https://webitechparis.com/"
           image={webitechImage}
-          description="Formation BTS SIO SLAM. Mon apprentissage portait sur le développement des applications web . J'ai acquis des compétences en JavaScript, React et SQL. <br/>J'ai beaucoup apprécié l'environnement d'apprentissage."
+          description="Formation BTS SIO SLAM. <br /> Mon apprentissage portait sur le développement des applications web . J'ai acquis des compétences en JavaScript, React et SQL."
         />
         <Card
           title="Vinci SA"
           link="https://www.vinci.com/"
           image={vinciImage}
-          description="Alternance Support applicatif. <br/> Mes responsabilités incluaient la résolution d'incidents et le support technique aux utilisateurs. J'ai amélioré mes compétences en résolution de problèmes et en communication. L'expérience chez Vinci m'a permis de travailler dans un grand environnement professionnel."
+          description="Alternance Support applicatif. <br /> Mes responsabilités incluaient la résolution d'incidents et le support technique aux utilisateurs. J'ai amélioré mes compétences en résolution de problèmes et en communication. L'expérience chez Vinci m'a permis de travailler dans un grand environnement professionnel."
         />
         <Card
           title="Notre Dame du Grand-Champ"
-          link="https://www.ndgc.fr/"
+          link="https://www.nd-grandchamp.fr/"
           image={ndgcImage}
-          description="Stage Technicienne Informatique. <br/> J'étais en charge de la maintenance du réseau informatique et de la gestion des serveurs. Ce stage m'a permis de développer mes compétences en administration système et en sécurité informatique. J'ai beaucoup appris sur les infrastructures informatiques."
+          description="Stage Technicienne Informatique. <br /> J'étais en charge de la maintenance du réseau informatique et de la gestion des serveurs. Ce stage m'a permis de développer mes compétences en administration système et en sécurité informatique. J'ai beaucoup appris sur les infrastructures informatiques."
         />
       </div>
     </div>
