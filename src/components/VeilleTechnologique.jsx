23942070aa122493ddfafd8c22187bab943a61d1
Samaholicc <samaholiccs@gmail.com>
2024-11-25 05:59:29 +0100

    final

--- src/components/VeilleTechnologique.jsx
+++ src/components/VeilleTechnologique.jsx
@@ -40,35 +40,13 @@ const VeilleTechnologique = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+  
   };
 
-  const NextArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={`${className} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full`}
-        style={{ ...style, display: "block", zIndex: 1 }}
-        onClick={onClick}
-      >
-        ›
-      </div>
-    );
-  };
+  
 
-  const PrevArrow = (props) => {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={`${className} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full`}
-        style={{ ...style, display: "block", zIndex: 1 }}
-        onClick={onClick}
-      >
-        ‹
-      </div>
-    );
-  };
+  
+
 
   return (
     <div id="VeilleTechnologique" className="w-full px-[12%] py-10 scroll-mt-20 text-center">
