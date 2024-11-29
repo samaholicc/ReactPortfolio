3d602ce4c65fbcd7a4e274432fbfb2610c1167d7
Samaholicc <samaholiccs@gmail.com>
2024-11-29 18:36:08 +0100

    modif

--- src/components/EcoleEntreprise.jsx
+++ src/components/EcoleEntreprise.jsx
@@ -65,7 +65,7 @@ const EcoleEntreprise = () => {
       {showModal && (
         <div style={modalStyles.overlay}>
           <div style={modalStyles.modal}>
-            <h3 style={modalStyles.modalTitle}>{Education.desc}</h3>
+            <h3 style={modalStyles.modalTitle}>{Entreprise.desc}</h3>
             <p>Voici les détails concernant les missions.</p>
             <button onClick={handleCloseModal} style={modalStyles.closeButton}>
               Fermer
