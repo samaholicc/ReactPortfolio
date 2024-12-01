ba0d22314f89c435122dc64381f1108688490987
Samaholicc <samaholiccs@gmail.com>
2024-12-01 18:51:15 +0100

    final

--- src/components/VeilleTechnologique.jsx
+++ src/components/VeilleTechnologique.jsx
@@ -92,8 +92,8 @@ const VeilleTechnologique = () => {
           />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-  {/* RSS Feed Section - left column */}
+        <div className="col-span-1 mb-10"> {/* Allow the RSS feed to occupy the remaining space */}
+        {/* RSS Feed Section - left column */}
   {isLoading ? (
     <p>Chargement des articles...</p>
   ) : error ? (
