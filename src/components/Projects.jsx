0b5e1a20ee1556447374590523738446fe043067
Samaholicc <samaholiccs@gmail.com>
2024-12-01 07:31:50 +0100

    modif

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
         Voici quelques-uns de mes projets récents, mettant en valeur mon expertise dans le développement web et d'applications mobiles.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 my-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
         {projects.map((project, index) => (
           <div key={index} className="flip-card">
             <div className="flip-card-inner">
