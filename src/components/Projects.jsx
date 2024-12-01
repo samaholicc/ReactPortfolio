1f00f6977d060f63c988e44aee4841f2f3fb02f3
Samaholicc <samaholiccs@gmail.com>
2024-12-01 10:26:00 +0100

    modif

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,9 +24,9 @@ const Projects = () => {
         Voici quelques-uns de mes projets récents, mettant en valeur mon expertise dans le développement web et d'applications mobiles.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 my-10 justify-items-between">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 my-10 justify-items-center">
         {projects.map((project, index) => (
-          <div key={index} className="flip-card w-96 h-96 relative">
+          <div key={index} className="flip-card w-64 h-64 relative">
             <div className="flip-card-inner">
               {/* Front Face */}
               <div className="flip-card-front card-content flex flex-col justify-between h-full bg-white rounded-lg shadow-md">
