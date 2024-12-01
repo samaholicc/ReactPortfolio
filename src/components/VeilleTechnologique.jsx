4408ea13be9356da6ffa251f85e7b25de7495e8e
Samaholicc <samaholiccs@gmail.com>
2024-12-01 18:49:15 +0100

    final

--- src/components/VeilleTechnologique.jsx
+++ src/components/VeilleTechnologique.jsx
@@ -92,7 +92,7 @@ const VeilleTechnologique = () => {
           />
         </div>
 
-        <div className="md:w-1/2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
   {/* RSS Feed Section - left column */}
   {isLoading ? (
     <p>Chargement des articles...</p>
